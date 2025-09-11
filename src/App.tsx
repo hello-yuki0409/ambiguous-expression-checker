ec379db85766ed86f013cfe5f216edccb45af9ce
hello-yuki0409 <bobu2784@gmail.com>
2025-09-11 22:59:07 +0900

    refactor: tailwindcssを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <></>;
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+      <p className="mt-4 text-gray-700">TailwindCSS が有効です 🎉</p>
+    </div>
+  );
 }
