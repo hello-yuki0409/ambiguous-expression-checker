eaf88aba5a5f20d7b3f20005d03cd4a54b3a97b1
hello-yuki0409 <bobu2784@gmail.com>
2025-09-12 23:58:13 +0900

    refactor: shadcn導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
-      <p className="mt-4 text-gray-700">TailwindCSS が有効です 🎉</p>
+      <h1 className="text-3xl font-bold">shadcn/ui test</h1>
+      <Button className="mt-4">Click me</Button>
     </div>
   );
 }
