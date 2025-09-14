acdeb3b37f2b0286a7b09e887963be78c576ed7d
hello-yuki0409 <bobu2784@gmail.com>
2025-09-14 18:02:32 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
     <div className="p-8">
       <h1 className="text-3xl font-bold">shadcn/ui test</h1>
       <Button className="mt-4">Click me</Button>
+      <Button className="mt-4">Click me 111！！</Button>
     </div>
   );
 }
